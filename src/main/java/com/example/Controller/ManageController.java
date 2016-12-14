bb82aed8dd79de27d57d959faaeff59418cc6603
石琦峰 <byhieg@gmail.com>
2016-12-15 01:29:02 +0800

    修改小bug

--- src/main/java/com/example/Controller/ManageController.java
+++ src/main/java/com/example/Controller/ManageController.java
@@ -1,5 +1,8 @@
 package com.example.Controller;
 
+import com.example.Model.Program;
+import com.example.Repository.ProgramRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequestMapping("/apiManage/index")
@@ -14,6 +18,9 @@ public class ManageController {
 
 //    private String userInfoname;
 //    private Gson gson = new Gson();
+
+    @Autowired
+    ProgramRepository programRepository;
 
 
     @RequestMapping(value = "/{username}",method = RequestMethod.GET)
@@ -23,7 +30,12 @@ public class ManageController {
         if(session.getAttribute(username) == null){
             return "redirect:/apiManage/login";
         }
+        List<Program> programs = programRepository.findAll();
         model.addAttribute("username", username);
+        model.addAttribute("name1", programs.get(0));
+        model.addAttribute("name2", programs.get(1));
+        model.addAttribute("name3", programs.get(2));
+        model.addAttribute("name4", programs.get(3));
         return "manage";
     }
 

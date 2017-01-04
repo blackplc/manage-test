e2bcf9f990dacc7aa707713fdaa2106790eb1ceb
石琦峰 <byhieg@gmail.com>
2017-01-05 00:41:57 +0800

    添加得到具体API信息的接口

--- src/main/java/com/example/Controller/ManageInfoController.java
+++ src/main/java/com/example/Controller/ManageInfoController.java
@@ -1,9 +1,12 @@
 package com.example.Controller;
 
+import com.example.Model.Api;
 import com.example.Model.Program;
+import com.example.Repository.ApiRepository;
 import com.example.Repository.ProgramRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,9 +24,20 @@ public class ManageInfoController {
     @Autowired
     ProgramRepository programRepository;
 
+    @Autowired
+    ApiRepository apiRepository;
+
     @RequestMapping(value = "/apiNames",method = RequestMethod.GET)
     @ResponseBody
     public List<Program> getApiName(){
         return programRepository.findAll();
     }
+
+    @RequestMapping(value = "/{apiID}/apiDetail",method = RequestMethod.GET)
+    @ResponseBody
+    public List<Api> getApiDetail(@PathVariable("apiID")String apiID){
+        return apiRepository.findAll();
+    }
+
+
 }
